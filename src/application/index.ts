3439850690c9a81cc7743a9159f77e89844ca4d0
Mattia-Carcione <mattia.carcione01@gmail.com>
2026-06-10 21:45:41 +0200

    refactor(application): wire claims identity

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,5 +1,4 @@
 export * from './cqrs/index'
-export * from './gate_keepers/index'
 export * from './loggers/index'
 export * from './mappers/index'
 export * from './specifications/index'
---
+++ src/application/mappers/claims-identity.mapper.ts
@@ -0,0 +1,17 @@
+import type { IBaseMapper, Identity } from '@/domain'
+import type { AuthClaims } from '@/shared'
+import { GuidHelper } from '@/shared'
+
+/**
+ * @description ClaimsIdentityMapper is responsible for mapping authentication claims (AuthClaims) to an Identity object. This mapper takes the claims extracted from a token (such as a JWT) and transforms them into a structured Identity that can be used throughout the application for authentication and authorization purposes. The mapping includes parsing the user ID and tenant ID from the claims, as well as extracting roles and permissions.
+ */
+export class ClaimsIdentityMapper implements IBaseMapper<AuthClaims, Identity> {
+  public map(claims: AuthClaims): Identity {
+    return {
+      userId: GuidHelper.parse(claims.sub),
+      tenantId: GuidHelper.parse(claims.tenantId),
+      roles: claims.roles,
+      permissions: claims.permissions,
+    }
+  }
+}
