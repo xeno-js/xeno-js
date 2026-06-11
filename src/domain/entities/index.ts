67c2401544bcd0b9376a00f2ce02f9f479fdddce
Mattia Carcione <133814979+Mattia-Carcione@users.noreply.github.com>
2026-06-11 19:39:21 +0000

    refactor(domain): reorganize domain layer part 7

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,1 +1,1 @@
-export type * from './ientity'
+export type * from './ientity.contracts'
--- src/domain/value_objects/index.ts
+++ src/domain/value_objects/index.ts
@@ -1,2 +1,2 @@
-export type * from './ivalue-object'
+export type * from './ivalue-object.contracts'
 export * from './value-object'
