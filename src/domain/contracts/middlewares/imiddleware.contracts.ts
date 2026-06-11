3f1109a9cd0b3c877824e3f36c2e97b38fbd185b
Mattia Carcione <133814979+Mattia-Carcione@users.noreply.github.com>
2026-06-11 19:41:56 +0000

    feat(domain): add domain contract definitions part 3

---
+++ src/domain/contracts/middlewares/imiddleware.contracts.ts
@@ -0,0 +1,13 @@
+import type { ResponseDto } from '@/shared'
+
+/**
+ * @description The IMiddleware interface defines the contract for middleware components that process incoming HTTP requests. Implementing classes must provide an execute method that takes an HttpRequest as input and returns a Promise of a ResponseDto, which can either be a successful response or an error response. This design allows for flexible middleware implementations that can perform various tasks such as authentication, logging, request transformation, or response generation.
+ */
+export interface IMiddleware<TRequest = unknown> {
+  /**
+   * @description The execute method processes an incoming HTTP request and returns a ResponseDto that can either be a successful response or an error response. This allows for flexible middleware implementations that can either modify the request, perform side effects, or generate a response directly.
+   * @param request The incoming HTTP request that the middleware will process. This object typically contains information such as the request method, URL, headers, body, and other relevant data needed for processing.
+   * @returns A Promise that resolves to a ResponseDto containing either a successful response or an error response. The ResponseDto allows for handling both success and error cases in a consistent manner.
+   */
+  execute<T>(req: TRequest, next: () => Promise<ResponseDto<T>>): Promise<ResponseDto<T>>
+}
